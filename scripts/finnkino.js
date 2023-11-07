966d38b89fd61598500951db95034ebb41081499
NicoJstudent <127749524+NicoJstudent@users.noreply.github.com>
2023-11-07 09:27:31 +0200

    Update finnkino.js
    Pientä hienosäätöä ja kommentointia selkeyden vuoksi.

--- scripts/finnkino.js
+++ scripts/finnkino.js
@@ -12,7 +12,7 @@ async function fetchDataAndParse() {
         const xmlDoc = parser.parseFromString(xmlString, 'text/xml');
         const showElements = xmlDoc.getElementsByTagName('Show');
 
-        for (let index = 0; index < 5; index++) {
+        for (let index = 0; index < 5; index++) {       //"showElements.length" indeksiin hakutuloksien määrä
             const showElement = showElements[index];
             const title = showElement.getElementsByTagName('Title')[0].textContent;
             const release = showElement.getElementsByTagName('dtLocalRelease')[0].textContent;
@@ -20,9 +20,9 @@ async function fetchDataAndParse() {
             const theatre = showElement.getElementsByTagName('Theatre')[0].textContent;
             const length = showElement.getElementsByTagName('LengthInMinutes')[0].textContent;
             const ShowURL = showElement.getElementsByTagName('ShowURL')[0].textContent;
-            const showImage = showElement.getElementsByTagName('EventMediumImagePortrait')[0].textContent;
+            const showImage = showElement.getElementsByTagName('EventMediumImagePortrait')[0].textContent;  //Small, Medium, Large, kuvien kokoihin
 
-            console.log(`Show ${index + 1}:`);
+            console.log(`Show ${index + 1}:`);  //Listauksen numero logia varten
             console.log('Title:', title);
             console.log('Ensi-ilta:', dateTrim(release));
             console.log('Rating:', rating);
@@ -37,7 +37,7 @@ async function fetchDataAndParse() {
     }
 }
 
-function dateTrim(date) {
+function dateTrim(date) {       //Poistaa päivämäärästä kellonajan
     return date.split('T')[0];
 }
 
