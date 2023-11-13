59aad49e1debe4ec345a30332e422e6e0b6e3aba
NicoJstudent <127749524+NicoJstudent@users.noreply.github.com>
2023-11-13 12:32:49 +0200

    Update tmdb.js
    Pelkästään haluttua dataa saadaan nyt ulos (vaatii lisää työtä, jotta on oikeaoppinen). Elokuvien genre_ids pitää muuttaa vastaamaan oikea tekstiä. Päivämäärän muutaminen suomalaiseen standardiin (dd/mm/yyyy)

--- unused/tmdb.js
+++ unused/tmdb.js
@@ -14,6 +14,23 @@ function testAuthentication() {
         .catch(err => console.error(err));
 }
 
+//    ---saatu data tässä muodossa---
+//
+//    adult: false,
+//    backdrop_path: '/8KJMOKyl9rpgb9ELEwhfBMIaTHw.jpg',
+//    genre_ids: [Array],
+//    id: 1139087,
+//    original_language: 'en',
+//    original_title: 'Once Upon a Studio',
+//    overview: '',
+//    popularity: 291.332,
+//    poster_path: '/hr8ZtwdbtjBquxlTCrczr685K5T.jpg',
+//    release_date: '2023-09-24',
+//    title: 'Once Upon a Studio',
+//    video: false,
+//    vote_average: 8.5,
+//    vote_count: 225
+
 
 
 function getToprated() {
@@ -22,26 +39,45 @@ function getToprated() {
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
-            return response.json();
-
+            return response.json()
         })
-        .then(data => {                   
+        .then(data => {
             if (data.results && Array.isArray(data.results)) {
                 data.results.forEach(movie => {
-                    console.log(movie.title);
+                    console.log(
+                        '---\n',
+                        '\n Elokuva:',movie.title,'\n',
+                        'ID:',movie.id,'\n',
+                        'Arvosana:',movie.vote_average,'\n',
+                        //'Kuvaus:',movie.overview,'\n',
+                        'Julkaistu:',movie.release_date,'\n',
+                        'Kieli:',movie.original_language,'\n',
+                        'genre_ids:',movie.genre_ids,'\n',
+                        'genre_ids:',getGenreID(movie.genre_ids),'\n',
+                        );
                 });
             }
         })
-        .catch(error => { 
+        .catch(error => {
             console.error(err);
-        }); 
+        });
 
 
-        //.then(response => response.json())
-        //.then(response => console.log(response))
-        //.catch(err => console.error(err));
+    //.then(response => response.json())
+    //.then(response => console.log(response))
+    //.catch(err => console.error(err));
 
 }
 
+function getGenreID() {
+    const 
+    fetch('https://api.themoviedb.org/3/genre/movie/list', options)
+        .then(response => response.json())
+        //.then(response => console.log(response))
+        .catch(err => console.error(err));
+    return 'action';
+}
+
+//getGenreID();
 getToprated();
 //testAuthentication();
