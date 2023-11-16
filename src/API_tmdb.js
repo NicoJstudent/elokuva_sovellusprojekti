013e12c57a818f7fa8d9090783f48a59f35f1b97
NicoJstudent <127749524+NicoJstudent@users.noreply.github.com>
2023-11-16 13:50:27 +0200

    API_tmdb.js
    - Hakee nyt genre listan ja vertaa genren ID:tä elokuvan ID:n kanssa ja listaa vastaavuudet tekstinä.
    
    - Kellonaika näkyy nyt dd/mm/yyyy muodossa.
    
    - Hieman siivosin ja kommentoin myös koodia selkeyden vuoksi.

--- src/API_tmdb.js
+++ src/API_tmdb.js
@@ -3,32 +3,49 @@ import axios from 'axios';
 
 //API avain täytyy salata
 const apiKey = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTI5MjQ1MGRjMDAzNTEwMjMzZWY3NDVmOWJkNWFhMiIsInN1YiI6IjY1NDhkMzI2ZDhjYzRhMDBlM2NjOTJkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pinVZ6Vu_Dy5u-YOpXG60nUsEssui8GV8TpBwEozhdE';
-
 const BaseUrl = 'https://api.themoviedb.org/3';
-const options = {   //Perus asetukset, voi lisätä/poistaa tarvittavia
+const options = {   //Perusasetukset, voi lisätä/poistaa tarvittavia
   method: 'GET',
-  params: {language: 'fi-FI'},    //fi-FI suomi, en-US englanti
+  params: { language: 'fi-FI' },    //fi-FI suomi, en-US englanti
   headers: {
     accept: 'application/json',
     Authorization: apiKey
-}
+  }
 };
-const generateApiUrl = (endpoint = {}) => { //Tekee endpointista osoitteen
+//Rakentaa endpointista osoitteen, ei tarvitse erikseen jokaista osoitetta
+const generateApiUrl = (endpoint = {}) => {
 
   return `${BaseUrl}${endpoint}`;
 };
 
 const APItmdb = () => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);   //Elokuva
+  const [genres, setGenres] = useState([]);   //Genre
 
-  const fetchData = async (endpoint) => {
+  const fetchData = async (endpoint) => { 
     try {
-      const response = await axios.get(generateApiUrl(endpoint), options);
-      setMovies(response.data.results);
-      
+      //Hakee elokuvat endpointin perusteella
+      const responseMovie = await axios.get(generateApiUrl(endpoint), options);
+      setMovies(responseMovie.data.results);
+
+      //Hakee genre listan
+      const responseGenre = await axios.get('https://api.themoviedb.org/3/genre/movie/list', options);
+      setGenres(responseGenre.data.genres);
+
     } catch (error) {
       console.error('Error fetching data:', error.message);
     }
+  };
+
+  //Vertaa ID ja palauttaa genren nimen
+  //Internetin perusteella jotkin listat "vanhentuneita", varalta "Unknown Genre", jos ei vastaavuuksia
+  const getGenreNames = (genreIds) => {
+    const genreNames = genreIds.map((genreId) => {
+      const genre = genres.find((gen) => gen.id === genreId);
+      return genre ? genre.name : 'Unknown Genre';  
+    });
+
+    return genreNames.join(', '); // Erottaa genret pilkulla
   };
 
   //**** Haluttuja tietoja saa näillä muuttujilla ****
@@ -46,9 +63,11 @@ const APItmdb = () => {
   //{movie.vote_count} (int) - Elokuvan äänestysmäärä
   //{movie.adult} (bool) - Onko elokuva aikuisille
 
+  //{new Date(movie.release_date).toLocaleDateString('fi-FI')} - Muuttaa päivämäärän muotoon DD/MM/YYYY
+
 
   return (  //Testausta, voi muokata haluamansa näköiseksi
-    <div> 
+    <div>
       <h1>Listaus testi</h1>
       <button onClick={() => fetchData('/movie/now_playing')}>Teattereissa</button>
       <button onClick={() => fetchData('/movie/popular')}>Suositut</button>
@@ -59,14 +78,15 @@ const APItmdb = () => {
         {movies.map(movie => (
           <li key={movie.id}>
             <p>Title: {movie.title}</p>
-            <p>Julkaisu: {movie.release_date}</p> 
+            <p>Julkaisu: {new Date(movie.release_date).toLocaleDateString('fi-FI')}</p>
             <p>Arvosana: {movie.vote_average}</p>
-            <p>Genre: {movie.genre_ids}</p>
+            <p>Genre: {getGenreNames(movie.genre_ids)}</p>
           </li>
         ))}
       </ul>
     </div>
   );
 };
+
 
 export default APItmdb;
