df198b3bcbcce48eab3d1e768a4a17fa0c8de2db
saanalapinkangas <118981440+saanalapinkangas@users.noreply.github.com>
2023-11-20 13:38:43 +0200

    uutiset lisätty

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Elokuvasivu from "./elokuvasivun_pohja";
 import Etusivu from "./etusivu";
 import Footer from "./footer";
 import Header from "./header";
+import Uutiset from "./uutiset";
 import Yhteiso from "./yhteiso";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
             <Route path="/" element={<Etusivu/>} />
             <Route path="/elokuvasivu" element={<Elokuvasivu/>} />
             <Route path="/yhteiso" element={<Yhteiso/>} />
+            <Route path="/uutiset" element={<Uutiset/>} />
         </Routes>
 
         <Footer />
