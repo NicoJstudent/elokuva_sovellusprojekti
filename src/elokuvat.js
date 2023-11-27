bb1ecb64980ca117bef3e85e2528c4e0ffdd7c93
saanalapinkangas <118981440+saanalapinkangas@users.noreply.github.com>
2023-11-27 10:45:04 +0200

    trailerihaku lisätty (imdb-linkin kautta)

--- src/elokuvat.js
+++ src/elokuvat.js
@@ -52,22 +52,22 @@ const Elokuvat = () => {
     //haetaan sivun aloitusnäkymä
     const fetchRandomMovies = async () => {
         try {
-          const response = await axios.get(generateApiUrl('/movie/popular'), options);
-      
-          // Sekoita elokuvien järjestys satunnaiseen järjestykseen.
-          const shuffledMovies = response.data.results.sort(() => Math.random() - 0.5);
-      
-          // Valitse 20 ensimmäistä satunnaista elokuvaa näytettäväksi.
-          const randomMovies = shuffledMovies.slice(0, 20);
-      
-          // Aseta satunnaisten elokuvien taulukko.
-          setMovies(randomMovies);
-        } catch (error) {
-          console.error('Error fetching random movies:', error.message);
-        }
-      };
+            const response = await axios.get(generateApiUrl('/movie/popular'), options);
 
-    useEffect (() => {
+            // Sekoita elokuvien järjestys satunnaiseen järjestykseen.
+            const shuffledMovies = response.data.results.sort(() => Math.random() - 0.5);
+
+            // Valitse 20 ensimmäistä satunnaista elokuvaa näytettäväksi.
+            const randomMovies = shuffledMovies.slice(0, 20);
+
+            // Aseta satunnaisten elokuvien taulukko.
+            setMovies(randomMovies);
+        } catch (error) {
+            console.error('Error fetching random movies:', error.message);
+        }
+    };
+
+    useEffect(() => {
         fetchRandomMovies();
     }, []);
 
@@ -75,8 +75,8 @@ const Elokuvat = () => {
         <>
             <div className='section'>
                 <h1>Elokuvat</h1>
-                
-                    <div className='luettelo filtterijono'>
+
+                <div className='luettelo filtterijono leveys90'>
                     <div className='leveys70'>
                         <div className='luettelo_osio'>
                             <h3>Suodata näkymää:</h3>
@@ -93,17 +93,17 @@ const Elokuvat = () => {
                         <div className='luettelo_osio'>
                             <button onClick={() => fetchData('/movie/upcoming')} className='yleinen_btn oranssi filtterit'>Tulossa</button>
                         </div>
-                        </div>
-                        <div className='leveys30'>
+                    </div>
+                    <div className='leveys30'>
                         <div className='luettelo_osio'>
                             <input type="text" placeholder="Etsi elokuva" className='tekstialue' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
                         </div>
                         <div className='luettelo_osio'>
                             <button onClick={searchMovies} className='yleinen_btn oranssi filtterit'>Etsi</button>
                         </div>
-                        </div>
                     </div>
-                
+                </div>
+
                 <Elokuvalistaus movies={movies} />
 
             </div>
@@ -111,20 +111,13 @@ const Elokuvat = () => {
     );
 };
 
-const Filtterit = ({ fetchData }) => {
-    return (
-        <>
-        </>
-    )
-}
-
 const Elokuvalistaus = ({ movies }) => {
     return (
         <>
             <div className='mediakorttien_runko'>
                 {movies?.map(movie => (
                     <div key={movie.id} className="mediakortti">
-                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="Elokuvan juliste." />
                         <h1>{new Date(movie.release_date).getFullYear()} &nbsp;&nbsp; <img src={star} style={{ width: '10px', height: 'auto', padding: '0px' }} alt="" /> {movie.vote_average?.toFixed(1)}</h1>
                         <h2><Link to={`${process.env.PUBLIC_URL}/elokuvasivu/${movie.id}`}>{movie.title}</Link></h2>
                     </div>
