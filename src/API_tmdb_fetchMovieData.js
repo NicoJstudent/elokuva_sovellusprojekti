e5d7510b494da4641e429d3399d1581203add95f
saanalapinkangas <118981440+saanalapinkangas@users.noreply.github.com>
2023-11-27 11:45:27 +0200

    hienosäätöä css

--- src/API_tmdb_fetchMovieData.js
+++ src/API_tmdb_fetchMovieData.js
@@ -1,21 +1,5 @@
 import axios from 'axios';
-
-//API avain täytyy salata
-const apiKey = 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzMTI5MjQ1MGRjMDAzNTEwMjMzZWY3NDVmOWJkNWFhMiIsInN1YiI6IjY1NDhkMzI2ZDhjYzRhMDBlM2NjOTJkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pinVZ6Vu_Dy5u-YOpXG60nUsEssui8GV8TpBwEozhdE';
-
-const BaseUrl = 'https://api.themoviedb.org/3';
-const options = {   //Perus asetukset, voi lisätä/poistaa tarvittavia
-    method: 'GET',
-    params: { language: 'fi-FI' },    //fi-FI suomi, en-US englanti
-    headers: {
-        accept: 'application/json',
-        Authorization: apiKey
-    }
-};
-const generateApiUrl = (endpoint = {}) => { //Tekee endpointista osoitteen
-
-    return `${BaseUrl}${endpoint}`;
-};
+import { generateApiUrl, options } from './APIkey';
 
 const fetchMovieData = async (movieId) => {
     try {
@@ -70,5 +54,5 @@ const fetchPersonData = async (personId) => {
 
 
 //export default fetchMovieData;
-export { fetchMovieAdditionalData, fetchMovieData, generateApiUrl };
+export { fetchMovieAdditionalData, fetchMovieData };
 
